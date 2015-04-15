d2a66b0368e62efc4abb50004bd6cb2331c5a15d
zengzheying <zengzheying@youmi.net>
2015-04-15 13:24:49 +0800

    添加新闻类别常量定义

--- server/views.py
+++ server/views.py
@@ -22,6 +22,28 @@ def login(req):
         return HttpResponse(Response.responseError(NOT_AVAILABLE_REQUEST_PARAMETERS, "无效的请求参数"))
 
 def getNews(req):
+    uid = req.GET.get('uid')
+    page = int(req.GET.get('page'))
+    cat = int(req.GET.get('cat'))
+    if cat == NEWS_CATALOG_RECOMMEND:  #推荐新闻
+        pass
+    elif cat == NEWS_CATALOG_CIVIL:  #国内新闻
+        pass
+    elif cat == NEWS_CATALOG_INTERNATIONAL:  #国际新闻
+        pass
+    elif cat == NEWS_CATALOG_SPORT:  #体育新闻
+        pass
+    elif cat == NEWS_CATALOG_ENTERTAINMENT:  #娱乐新闻
+        pass
+    elif cat == NEWS_CATALOG_ENVIRONMENTAL:  #环保新闻
+        pass
+    elif cat == NEWS_CATALOG_SOCIETY:  #社会新闻
+        pass
+    elif cat == NEWS_CATALOG_SCIENTIST:  #科技新闻
+        pass
+    elif cat == NEWS_CATALOG_FINANCE:  #财经新闻
+        pass
+
     news = SinaNews.objects.all()[:10]
     return HttpResponse(Response.response({"list": list(news)}))
 
